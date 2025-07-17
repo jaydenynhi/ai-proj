7526365c9edb41d40618513681883ba99f2fa5d2
y nhi <ynhinguyen@ys-MacBook-Pro.local>
2025-07-17 12:59:24 +0700

    added pdf conversion in routes/file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(
+    docs_url="/backend"
+)
+
+
--- app/routes/file.py
+++ app/routes/file.py
@@ -1,1 +1,16 @@
 # to read different types of files 
+from fastapi import APIRouter, UploadFile, File, HTTPException, Query
+from PyPDF2 import PdfReader
+from docx import Document
+import io, tempfile
+
+router = APIRouter()
+
+@router.post("/upload")
+async def upload_file(f: UploadFile = File(...)):
+    content = await f.read()
+
+    if f.content_type == "application/pdf" or f.filename.endswith(".pdf"):
+        reader = PdfReader(io.BytesIO(content))
+        pages = [p.extract_text() for p in reader.pages]
+        full_text = "\n\n".join(filter(None, pages))
